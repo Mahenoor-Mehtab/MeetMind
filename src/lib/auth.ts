bea8f8ee81667bdc5a16db2c4f5a305d5a399c78
Mahenoor Mehtab <mahenoormehtab31@gmail.com>
2026-06-04 02:13:04 +0530

    fix:auth configuration validation

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,15 +3,31 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db"; // your drizzle instance
 import * as schema from "@/db/schema";
 
+if (!process.env.GITHUB_CLIENT_ID) {
+throw new Error("Missing GITHUB_CLIENT_ID");
+}
+
+if (!process.env.GITHUB_CLIENT_SECRET) {
+throw new Error("Missing GITHUB_CLIENT_SECRET");
+}
+
+if (!process.env.GOOGLE_CLIENT_ID) {
+throw new Error("Missing GOOGLE_CLIENT_ID");
+}
+
+if (!process.env.GOOGLE_CLIENT_SECRET) {
+throw new Error("Missing GOOGLE_CLIENT_SECRET");
+}
+
 export const auth = betterAuth({
    socialProviders: {
         github: { 
-            clientId: process.env.GITHUB_CLIENT_ID as string, 
-            clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+            clientId: process.env.GITHUB_CLIENT_ID, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET , 
         }, 
          google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID as string, 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+            clientId: process.env.GOOGLE_CLIENT_ID ,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
         }, 
     },
     emailAndPassword: { 
